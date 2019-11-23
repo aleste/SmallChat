1d610617869eabd9c5e8aaa6a4342a8b56d725d1
Alejandro Steinmetz <asteinmetz78@gmail.com>
2019-11-23 13:43:59 -0300

    First pages

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">
+          {{ title }}
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+  computed: {
+    title() {
+      const currentPath = this.$route.fullPath
+      if (currentPath === '/') return 'SmallChat'
+      else if (currentPath === '/chat') return 'Chat'
+      else if (currentPath === '/auth') return 'Login'
+    },
+  },
+  data() {
+    return {
+      leftDrawerOpen: false,
+    }
+  },
+}
+</script>
---
+++ src/pages/Users.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="flex">
+    Usuarios
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
